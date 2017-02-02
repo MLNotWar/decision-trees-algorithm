4994af942f2e07a46b88807e3780f0a156f37633
Saturnin Pugnet <sp5414@point27.doc.ic.ac.uk>
2017-02-02 18:39:20 +0000

    merged with karim

--- src/tree_builder.py
+++ src/tree_builder.py
@@ -2,21 +2,38 @@ import numpy as np
 
 POSSIBLE_VALUES = 2
 
+class Tree:
+    def __init__(self, data=None):
+        self.children = []
+        self.data = data
+    def add_child(self, child):
+        self.children.append(child)
+
 # returns a decision tree for a given target label
 def decision_tree_learning(examples, attributes, binary_targets):
-    if(all_equal(binary_targets)):
-        # return leaf
+    if(all(binary_targets == binary_targets[0])):
+        return Tree(binary_targets[0])
     elif(attributes.size() == 0):
-        return new leaf(majority_value(binary_targets))
+        return Tree(majority_value(binary_targets))
     else:
-        best_attribute = choose_best_decision_attribute(examples, attibutes, binary_targets)
-        tree = new tree
+        best_attribute = choose_best_decision_attribute(examples, attributes, binary_targets)
+        tree = Tree(best_attribute)
         for i in range(POSSIBLE_VALUES)
-            tree.add(i)
+            subtree = Tree()
+            tree.add_child(subtree)
             new_examples, new_binary_targets = get_revelant_examples(examples, attributes, binary_targets, i)
             if(new_examples.size() == 0):
-                return new leaf(majority_value(binary_targets))
+                return Tree(majority_value(binary_targets))
             else:
-                new_attributes = attributes.remove(best_attribute)
-                tree.subtree(i) = decision_tree_learning(new_examples, new_attributes, new_binary_targets)
+                new_attributes = attributes.remove(best_attribute) #TODO
+                subtree = decision_tree_learning(new_examples, new_attributes, new_binary_targets)
     return tree
+
+def majority_value(binary_values):
+    #TODO
+
+def choose_best_decision_attribute(examples, attributes, binary_targets):
+    #TODO
+
+def get_revelant_examples(examples, attributes, binary_targets, i):
+    #TODO
