d1e2dc224b7b922d39f0f8f21affe39985769315
Hongjiang Liu <Astral9@users.noreply.github.com>
2017-02-02 18:01:31 +0000

    Remove hard-coded data file path

--- src/loader.py
+++ src/loader.py
@@ -1,22 +1,18 @@
 from scipy.io import loadmat
 
-def load_clean_data():
-    data = loadmat('data/cleandata_students.mat')
-    return data['x'], data['y']
-
-def load_noisy_data():
-    data = loadmat('data/noisydata_students.mat')
+def load_data(data_file):
+    data = loadmat(data_file)
     return data['x'], data['y']
 
 if __name__ == '__main__':
     print('Clean Data:')
-    x, y = load_clean_data()
+    x, y = load_data('data/cleandata_students.mat')
     print('x:', x)
     print('y:', y)
 
     print()
 
     print('Noisy Data:')
-    x, y = load_noisy_data()
+    x, y = load_data('data/noisydata_students.mat')
     print('x:', x)
     print('y:', y)
