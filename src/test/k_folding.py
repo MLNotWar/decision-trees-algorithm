f879fe11346fb678496a59a4d9c3aa27c0ba6ece
Saturnin Pugnet <sp5414@line24.doc.ic.ac.uk>
2017-02-08 20:35:27 +0000

    evaluate implemented

--- src/test/k_folding.py
+++ src/test/k_folding.py
@@ -5,10 +5,10 @@ class KFoldTest(Test):
         super.__init__(examples, binary_targets)
 
         self.n_folds = n_folds
-        self.training_masks = self.split_examples()
+        self.testing_masks = self.split_examples()
 
     def split_examples(self):
-        training_masks = []
+        testing_masks = []
         size, _ = self.examples.shape
         n_examples_per_fold = size / self.n_folds
 
@@ -21,7 +21,24 @@ class KFoldTest(Test):
             else:
                 mask[start, start + n_examples_per_fold] = True
 
-            training_masks.append(mask)
-        return training_masks
+            testing_masks.append(mask)
+        return testing_masks
 
     def evaluate(self, algorithm):
+        for i in range(self.n_folds):
+            testing_mask = self.testing_masks[i]
+            algorithm.fit(examples[~testing_mask], binary_targets[~testing_mask])
+            result = algorithm.predict(examples[testing_mask])
+            self._update_ftp(result, binary_targets[~testing_mask])
+
+    def _update_ftp(self, prediction, actual):
+        # TODO check validity of for loop
+        for i in range(prediction[0])
+            if(prediction[0][i] == 1 && prediction[0][i] == 1):
+                self.n_true_positives += 1
+            elif(prediction[0][i] == 1 && prediction[0][i] == 0):
+                self.n_false_positives += 1
+            elif(prediction[0][i] == 0 && prediction[0][i] == 0):
+                self.n_true_negatives += 1
+            elif(prediction[0][i] == 0 && prediction[0][i] == 1):
+                self.n_false_negatives += 1
