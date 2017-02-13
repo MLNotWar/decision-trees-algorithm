7eaa298e0c0980d51dfac05ed31c33b49ccc0c74
KarimNahas <kn1114@ic.ac.uk>
2017-02-13 16:37:39 +0000

    Updated commands

--- src/loader.py
+++ src/loader.py
@@ -18,18 +18,19 @@ if __name__ == '__main__':
     parser.add_argument("data", type=str, help="the data file")
     parser.add_argument("-v", dest="visualisation", help="visualisation", action="store_true")
     parser.add_argument("-t", dest="test", action="store_true")
+    parser.add_argument("-p", dest="prune", action="store_true")
 
     args = parser.parse_args()
 
     examples, binary_targets = load_data(args.data)
+    builder = BasicTreeBuilder() if not args.prune else PrunedTreeBuilder()
 
     if args.test:
         test = KFoldTest(examples, binary_targets)
-        confusion_matrix = test.evaluate(BasicTreeBuilder())
+        confusion_matrix = test.evaluate(builder)
         pprint.pprint(confusion_matrix.generate_report())
     else:
-        algorithm = BasicTreeBuilder()
-        algorithm.fit(examples, binary_targets)
+        trees = builder.build_trees(examples, binary_targets)
 
         trees = {k: v.to_data() for k, v in algorithm.trees.items()}
         if args.visualisation:
